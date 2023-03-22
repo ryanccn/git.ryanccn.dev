f57a34aeb78a24f4213f32fed86ecdbb925217e2
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-03-22 23:52:59 +0800

    fix(ci): install dependencies in deploy

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ const app = new Hono();
 
 app.get('/', (c) => c.redirect(`https://github.com/ryanccn`));
 app.get('/:repo', (c) => {
-  const repo = c.req.param('repo');
-  return c.redirect(`https://github.com/ryanccn/${repo}`);
+	const repo = c.req.param('repo');
+	return c.redirect(`https://github.com/ryanccn/${repo}`);
 });
 
 export default app;
