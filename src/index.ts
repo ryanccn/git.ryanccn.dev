bd43c69839ef07e6c407c81b037fb80f742b13e1
Ryan Cao <70191398+ryanccn@users.noreply.github.com>
2023-03-22 23:48:33 +0800

    feat: initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/', (c) => c.redirect(`https://github.com/ryanccn`));
+app.get('/:repo', (c) => {
+  const repo = c.req.param('repo');
+  return c.redirect(`https://github.com/ryanccn/${repo}`);
+});
+
+export default app;
